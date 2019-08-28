d3f5e1eb9dcacb5f12740031a7b8585aced99e8d
ouwen.huang@duke.edu <ouwen.huang@duke.edu>
2019-08-28 11:52:50 -0400

    Added more documentation

--- setup.py
+++ setup.py
@@ -4,10 +4,48 @@ from setuptools import setup
 REQUIRED_PACKAGES = ['pandas', 'scipy', 'polarTransform', 'numpy', 'tensorflow']
 
 setup(
-    name='trainer',
-    version='0.1',
+    name='MimickNet',
+    version='0.0.1',
     install_requires=REQUIRED_PACKAGES,
     packages=find_packages(),
     include_package_data=True,
-    description='My training application package.'
+    description='''
+    	Deep Learning Post-Processor for Creating Clinical Grade Ultrasound.'
+    	@article{DBLP:journals/corr/abs-1908-05782,
+		  author    = {Ouwen Huang and
+		               Will Long and
+		               Nick Bottenus and
+		               Gregg E. Trahey and
+		               Sina Farsiu and
+		               Mark L. Palmeri},
+		  title     = {MimickNet, Matching Clinical Post-Processing Under Realistic Black-Box
+		               Constraints},
+		  journal   = {CoRR},
+		  volume    = {abs/1908.05782},
+		  year      = {2019},
+		  url       = {http://arxiv.org/abs/1908.05782},
+		  archivePrefix = {arXiv},
+		  eprint    = {1908.05782},
+		  timestamp = {Mon, 19 Aug 2019 13:21:03 +0200},
+		  biburl    = {https://dblp.org/rec/bib/journals/corr/abs-1908-05782},
+		  bibsource = {dblp computer science bibliography, https://dblp.org}
+		}
+    ''',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries',
+    ],
+    license='Apache 2.0',
+    author='Ouwen Huang',
+    author_email='ouwen.huang@duke.edu',    
 )
