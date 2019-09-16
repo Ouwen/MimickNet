e7fb72a12111cfb733d8882eba601edeadc3fba6
Ouwen Huang <ouwen.huang@duke.edu>
2019-09-16 04:30:33 +0000

    Added gcp

--- submit_gcp.sh
+++ submit_gcp.sh
@@ -5,6 +5,19 @@ REGION=us-east1
 docker build -f Dockerfile -t $IMAGE_URI .
 docker push $IMAGE_URI
 
+JOB_NAME=MimickNet_Blackbox_$(date +%Y_%m_%d_%H%M%S)
+JOB_DIR=$GCS_BUCKET"/"$JOB_NAME
+gcloud ai-platform jobs submit training $JOB_NAME \
+  --master-image-uri $IMAGE_URI \
+  --scale-tier custom \
+  --master-machine-type standard_p100 \
+  --region $REGION \
+  --job-dir $JOB_DIR \
+  -- \
+  --train_das_csv 'gs://duke-research-us/mimicknet/data/training_a-v2.csv' \
+  --train_clinical_csv 'gs://duke-research-us/mimicknet/data/training_b-v2.csv'
+gcloud ai-platform jobs describe $JOB_NAME
+
 JOB_NAME=MimickNet_Blackbox_Phantom$(date +%Y_%m_%d_%H%M%S)
 JOB_DIR=$GCS_BUCKET"/"$JOB_NAME
 gcloud ai-platform jobs submit training $JOB_NAME \
@@ -29,4 +42,17 @@ gcloud ai-platform jobs submit training $JOB_NAME \
   -- \
   --train_das_csv 'gs://duke-research-us/mimicknet/data/training-v2-verasonics.csv' \
   --train_clinical_csv 'gs://duke-research-us/mimicknet/data/training-v2-clinical.csv'
+gcloud ai-platform jobs describe $JOB_NAME
+
+JOB_NAME=MimickNet_Blackbox_VerasonicsOnly$(date +%Y_%m_%d_%H%M%S)
+JOB_DIR=$GCS_BUCKET"/"$JOB_NAME
+gcloud ai-platform jobs submit training $JOB_NAME \
+  --master-image-uri $IMAGE_URI \
+  --scale-tier custom \
+  --master-machine-type standard_p100 \
+  --region $REGION \
+  --job-dir $JOB_DIR \
+  -- \
+  --train_das_csv 'gs://duke-research-us/mimicknet/data/training-v2-verasonics_a.csv' \
+  --train_clinical_csv 'gs://duke-research-us/mimicknet/data/training-v2-verasonics_b.csv'
 gcloud ai-platform jobs describe $JOB_NAME
