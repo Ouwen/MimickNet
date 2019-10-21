e9988fd05f4f9f9e60a3a6983325b75f34e9586b
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-21 14:47:24 +0000

    Added noclip model version

--- trainer/callbacks/get_csv_metrics.py
+++ trainer/callbacks/get_csv_metrics.py
@@ -20,11 +20,14 @@ import tensorflow as tf
 import numpy as np
 from skimage.transform import match_histograms
 from trainer import utils
+import sys
 
 from multiprocessing import Pool
 
 _FIELDNAMES = ['filename', 'mse', 'mae', 'ssim', 'contrast_structure', 'luminance', 'psnr', 'corr',
-               'iq_mse', 'iq_mae', 'iq_ssim', 'iq_contrast_structure', 'iq_luminance', 'iq_psnr', 'iq_corr']
+               'iq_mse', 'iq_mae', 'iq_ssim', 'iq_contrast_structure', 'iq_luminance', 'iq_psnr', 'iq_corr',
+              'nh_mse', 'nh_mae', 'nh_ssim', 'nh_contrast_structure', 'nh_luminance', 'nh_psnr', 'nh_corr', 
+              'nhiq_mse', 'nhiq_mae', 'nhiq_ssim', 'nhiq_contrast_structure', 'nhiq_luminance', 'nhiq_psnr', 'nhiq_corr']
 
 class GetCsvMetrics(tf.keras.callbacks.Callback):
     def __init__(self, forward, dataset, job_dir, count, out='metrics'):
@@ -63,13 +66,21 @@ class GetCsvMetrics(tf.keras.callbacks.Callback):
                 dtce = dtce.numpy()
 
                 output = self.forward.predict(iq)
-                iq = match_histograms(iq, dtce)
-                output = match_histograms(output, dtce)
-
+                output = np.clip(output, 0, 1)
                 dtce_metrics = self.get_metrics(dtce, output)
                 iq_metrics = self.get_metrics(dtce, iq)
 
                 tmp = {}
+                for key, value in iq_metrics.items():
+                    tmp['nhiq_{}'.format(key)] = value
+                for key, value in dtce_metrics.items():
+                    tmp['nh_{}'.format(key)] = value
+
+                dtce = match_histograms(dtce, iq)
+                output = match_histograms(output, iq)
+                dtce_metrics = self.get_metrics(dtce, output)
+                iq_metrics = self.get_metrics(dtce, iq)
+
                 for key, value in iq_metrics.items():
                     tmp['iq_{}'.format(key)] = value
 
@@ -77,10 +88,10 @@ class GetCsvMetrics(tf.keras.callbacks.Callback):
                 combined['filename'] = filename
                 self.writer.writerow(combined)
                 print('{}/{} - {}'.format(i, self.count, filename))
-            except StopIteration:
+            except:
                 print('End of test data')
                 break
-        
+
     def on_train_end(self, logs={}):
         print('Running final validation metrics')
         self.writer = csv.DictWriter(self.csv_out_filepath, fieldnames=_FIELDNAMES)
@@ -92,19 +103,21 @@ class GetCsvMetrics(tf.keras.callbacks.Callback):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--forward', default='./examples/mimicknet.h5', help='Forward .hdf5 model filepath')
+    parser.add_argument('--forward', default='./examples/models/mimicknet_1568473738-210304.h5', help='Forward .hdf5 model filepath')
     parser.add_argument('--job_dir', default='.', help='Metrics ouptut filepath')
     parser.add_argument('--csv', default='gs://duke-research-us/mimicknet/data/testing-v2.csv')
-    parser.add_argument('--out', default='metrics', help='Metrics ouptut filepath')
-
+    parser.add_argument('--out', default='mimicknet_1568473738-210304_metrics', help='Metrics ouptut filepath')
+    parser.add_argument('--clip', default=None)
+    
     args = parser.parse_args()
+    if args.clip is not None:
+        args.clip = (float(args.clip), 0)
         
     model = tf.keras.models.load_model(args.forward)
     test_dataset, test_count = utils.MimickDataset(
-        clipping=(-80,0)
+        clipping=args.clip
     ).get_paired_ultrasound_dataset(csv=args.csv, batch_size=1)
 
     get_csv_metrics_data_callback = GetCsvMetrics(model, test_dataset, args.job_dir, count=test_count, out=args.out)
     get_csv_metrics_data_callback.on_train_end()
-    get_csv_metrics_data_callback.csv_out_filepath.flush()
-    get_csv_metrics_data_callback.csv_out_filepath.close()
+
--- trainer/utils/custom_ssim.py
+++ trainer/utils/custom_ssim.py
@@ -21,7 +21,7 @@ from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn
 from tensorflow.python.ops import nn_ops
-from tensorflow.image import convert_image_dtype
+import tensorflow as tf
 
 def _verify_compatible_image_shapes(img1, img2):
     """Checks if two image tensors are compatible for applying SSIM or PSNR.
@@ -147,9 +147,9 @@ def ssim(img1, img2, max_val):
     # Need to convert the images to float32.  Scale max_val accordingly so that
     # SSIM is computed correctly.
     max_val = math_ops.cast(max_val, img1.dtype)
-    max_val = convert_image_dtype(max_val, dtypes.float32)
-    img1 = convert_image_dtype(img1, dtypes.float32)
-    img2 = convert_image_dtype(img2, dtypes.float32)
+    max_val = tf.image.convert_image_dtype(max_val, dtypes.float32)
+    img1 = tf.image.convert_image_dtype(img1, dtypes.float32)
+    img2 = tf.image.convert_image_dtype(img2, dtypes.float32)
     ssim_per_channel, cs, luminance = _ssim_per_channel(img1, img2, max_val)
     # Compute average over color channels.
     return math_ops.reduce_mean(ssim_per_channel, [-1]), math_ops.reduce_mean(cs, [-1]), math_ops.reduce_mean(luminance, [-1])
